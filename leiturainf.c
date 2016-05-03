fd787c88ede96260f017f14cf3701924bcce03e0
wanderson neves <nevesswanderson@gmail.com>
2016-05-03 00:44:29 +0000

    Imprimir maior e menor número

---
+++ leiturainf.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main()
+{
+    int maiornum=-1, menornum=999999, opcao, num;
+    
+    do
+    {
+    
+    printf("\nEntre com um número:\n");
+    scanf("%d", &num);
+    
+         if (num > maiornum)
+         
+             maiornum = num;
+         
+         if(num < menornum)
+         
+             menornum = num;
+         
+   
+    
+    printf("\nDeseja continuar?\n1 - Sim\n2 - Não\n");
+    scanf("%d", &opcao);
+    }
+    while (opcao==1);
+   
+        
+       
+        printf("Maior número foi: %d\n", maiornum);
+        printf("Menor número foi: %d", menornum);
+    
+    
+   
+    
+    
+    
+    return 0;
+}
